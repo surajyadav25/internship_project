33b1c682bea9390c3a3a7f25327494e0bea33ff4
Suraj Yadav <surajyadav@surajs-air.home>
2023-07-29 02:55:49 -0400

    update2

--- db.js
+++ db.js
@@ -1,30 +1,3 @@
-// // db.js
-// const { MongoClient } = require('mongodb');
-
-// const databaseUrl = 'mongodb+srv://sysurajyadav25:admin@cluster0.lnktjem.mongodb.net/?retryWrites=true&w=majority'; // Replace this with your actual connection string
-
-// async function connectToDatabase() {
-//   try {
-//     const client = await MongoClient.connect(databaseUrl, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//       // Add the following line to enable debugging
-//       // debug: true
-//     });
-
-//     console.log('Connected to MongoDB database');
-//     return client.db(); // Return the database object to use it in your app
-//   } catch (error) {
-//     console.error('MongoDB connection error:', error);
-//     throw error;
-//   }
-// }
-
-
-// module.exports = { connectToDatabase };
-
-
-// db.js
 require('dotenv').config();
 const mongoose = require('mongoose');
 const { MONGODB_URI } = process.env;
