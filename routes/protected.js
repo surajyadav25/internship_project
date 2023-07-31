fde32908cf4a0ea94f240ebe1f5c7495933c107b
Suraj Yadav <surajyadav@Surajs-MacBook-Air.local>
2023-07-31 13:39:51 -0400

    post changed to get

--- routes/protected.js
+++ routes/protected.js
@@ -4,7 +4,7 @@ const authMiddleware = require('../middleware/authMiddleware');
 const rideController = require('../controller/ride')
 
 router.post('/ride/create', authMiddleware, rideController.createRide);
-router.post('/ride/findride', authMiddleware, rideController.getRides);
+router.get('/ride/findride', authMiddleware, rideController.getRides);
 
 
 router.get('/ride/offeredRides', authMiddleware, rideController.offeredRides);
